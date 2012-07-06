99c7a0facd366ca78d0a09a51ac563c30f34b9f8
Roman Joost <roman@bromeco.de>
2012-07-06 19:51:31 +1000

    Bootstrapping static analysis tool to slice programs

---
+++ setup.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='programslice',
+      version=version,
+      description="Statistical analysis tool",
+      long_description="""\
+Slice programs""",
+      keywords='',
+      author='Róman Joost',
+      author_email='roman@bromeco.de',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      extras_require=dict(
+          test=[]
+      ),
+     )

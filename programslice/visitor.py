41df6ffc61b922250f5b5395584ab10afbf76100
Roman Joost <roman@bromeco.de>
2012-07-11 13:10:54 +1000

    Added binary operation handler.
    I think this is dodgy. Will need a few more tests, as I don't think we
    can rely that left and right operands are ast.Names.

--- programslice/visitor.py
+++ programslice/visitor.py
@@ -9,13 +9,17 @@ class DataDependencyVisitor(ast.NodeVisitor):
     def visit_Assign(self, node):
         self.context = node.targets[0].id
         for c in ast.iter_child_nodes(node):
-            if c in node.targets:
-                self.context = c.id
             meth = getattr(self, 'visit_' + c.__class__.__name__,
                            self.generic_visit)
             meth(c)
 
+    def visit_BinOp(self, node):
+        for n in [node.left, node.right]:
+            self.visit_Name(n)
+
     def visit_Name(self, node):
         if node.id not in self.graph.keys():
             self.graph.setdefault(node.id, [node.lineno])
+        else:
+            self.graph[node.id].append(node.lineno)
 

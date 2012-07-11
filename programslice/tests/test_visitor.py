fe0d48ce040211175ea4bef7ad76cd92f470ca64
Roman Joost <roman@bromeco.de>
2012-07-11 12:01:27 +1000

    Bootstrapping node traversal

--- programslice/tests/test_visitor.py
+++ programslice/tests/test_visitor.py
@@ -6,9 +6,15 @@ import unittest2
 class TestDataDependencyVisitor(unittest2.TestCase):
 
     def setUp(self):
-        self.tree = ast.Assign([ast.Name('foo'), ast.Store()],
-                               ast.Name('foo', ast.Load()))
+        self.node = ast.Assign([ast.Name('foo', ast.Store())],
+                               ast.Name('bar', ast.Load()))
+        self.node.lineno = 1
+        self.col_offset = 0
+        self.node.targets[0].lineno = 1
+        self.node.value.lineno = 1
 
     def test_visit_Assign(self):
         visitor = programslice.visitor.DataDependencyVisitor()
-        visitor.visit_Assign(self.tree)
+        visitor.visit_Assign(self.node)
+        self.assertEqual([1], visitor.graph['foo'])
+        self.assertEqual([1], visitor.graph['bar'])
--- programslice/visitor.py
+++ programslice/visitor.py
@@ -7,4 +7,15 @@ class DataDependencyVisitor(ast.NodeVisitor):
         self.graph = dict()
 
     def visit_Assign(self, node):
-        return node
+        self.context = node.targets[0].id
+        for c in ast.iter_child_nodes(node):
+            if c in node.targets:
+                self.context = c.id
+            meth = getattr(self, 'visit_' + c.__class__.__name__,
+                           self.generic_visit)
+            meth(c)
+
+    def visit_Name(self, node):
+        if node.id not in self.graph.keys():
+            self.graph.setdefault(node.id, [node.lineno])
+

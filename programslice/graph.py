1f3696e6435e8005878b6960a74e823bc8b56d24
Roman Joost <roman@bromeco.de>
2012-07-12 20:27:51 +1000

    Refactored the graph again.
    The previous implementation was a no-brainer.

--- programslice/graph.py
+++ programslice/graph.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import OrderedDict
 
 
 class Node(object):
@@ -12,32 +12,18 @@ class Node(object):
 
 class Graph(object):
 
-    def __init__(self):
-        self.graph = dict()
+    def __init__(self, name=''):
+        self.name = name
+        self.graph = OrderedDict()
 
-    def edges(self, name):
-        result = []
-        if name in self.graph.keys():
-            visited = []
-            children = deque(self.graph[name])
-            while children:
-                child = children.popleft()
-                if child.next:
-                    new = deque(self.graph[child.next.name])
-                    children.extend(new)
-                result.append(child.lineno)
-                if child in visited:
-                    break
-                visited.append(child)
-        return result
+    def edges(self):
+        return [x.lineno for x in self.graph.keys()]
 
     def add(self, node):
-        if not node.name in self.graph.keys():
-            self.graph.setdefault(node.name, [])
-        self.graph[node.name].append(node)
+        self.graph.setdefault(node, [])
 
     def connect(self, n1, n2):
-        self.graph[n1][0].next = self.graph[n2][0]
+        self.graph.setdefault(n1, []).append(n2)
 
     def __len__(self):
         return len(self.graph)
--- programslice/tests/test_graph.py
+++ programslice/tests/test_graph.py
@@ -11,28 +11,30 @@ class TestGraph(unittest2.TestCase):
     def test_add(self):
         self.graph.add(Node('A', 1))
         self.graph.add(Node('A', 2))
-        self.assertEqual([1, 2], self.graph.edges('A'))
-        self.assertFalse(self.graph['A'][0].next)
+        self.assertEqual([1, 2], self.graph.edges())
 
         self.graph.add(Node('B', 3))
-        self.assertTrue(len(self.graph) == 2)
-        self.assertEqual([3], self.graph.edges('B'))
+        self.assertEqual(3, len(self.graph))
+        self.assertEqual([1, 2, 3], self.graph.edges())
 
     def test_connect(self):
         self.graph.add(Node('A', 1))
         self.graph.add(Node('B', 1))
         self.graph.connect('A', 'B')
-        self.assertEqual('B', self.graph['A'][0].next.name)
+        self.assertEqual(['B'], self.graph['A'])
 
         self.graph.connect('B', 'A')
-        self.assertEqual('A', self.graph['B'][0].next.name)
+        self.assertEqual(['A'], self.graph['B'])
 
     def test_edges(self):
-        self.graph.add(Node('A', 1))
-        self.graph.add(Node('A', 2))
-        self.graph.add(Node('B', 3))
-        self.graph.connect('A', 'B')
-        self.graph.connect('B', 'A')
+        n1 = Node('A', 1)
+        n2 = Node('A', 2)
+        n3 = Node('B', 3)
+        self.graph.add(n1)
+        self.graph.add(n2)
+        self.graph.add(n3)
+        self.graph.connect(n1, n2)
+        self.graph.connect(n2, n3)
 
-        result = self.graph.edges('A')
-        self.assertEqual([1, 2, 3, 1], result)
+        result = self.graph.edges()
+        self.assertEqual([1, 2, 3], result)

09e0d8f610c5ae93507a4f3aa7d14e595f675952
Roman Joost <roman@bromeco.de>
2012-07-25 09:00:41 +1000

    Forgot to add an edge. Fixes test

--- programslice/tests/test_visitor.py
+++ programslice/tests/test_visitor.py
@@ -29,7 +29,7 @@ class TestControlDependencyVisitor(unittest2.TestCase):
         self.visitor.visit(node)
         graph1, graph2 = self.visitor.graphs
         self.assertEqual([3], graph1.edges)
-        self.assertEqual([4, 5, 6], graph2.edges)
+        self.assertEqual([4, 5, 6, 7], graph2.edges)
 
     def test_visit_While(self):
         node = self.load_testdata('binsearch.py')

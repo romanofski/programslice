daf97a49c8095e5c1c07009cc826de010784f85a
Roman Joost <roman@bromeco.de>
2012-07-28 20:39:07 +1000

    Check for encoding declarations and shebangs

--- programslice/__init__.py
+++ programslice/__init__.py
@@ -1,13 +1,23 @@
 import ast
+import re
 import programslice.visitor
 
 
-def slice_vim_buffer(currentline, buffer, name):
+def slice_vim_buffer(currentline, contents, name):
     """
     Utility function which can be used for integrating with vim
     """
-    node = ast.parse(buffer, name)
+    lines = []
+    # catch encoding declarations and shebangs
+    head = re.compile(r'#!\/.*\n#.*coding[:=]\s*([-\w.]+)')
+    encoding = (head.match(contents).group(1)
+                if head.match(contents) else u'utf-8')
+    contents = head.sub('', contents)
+
+    node = ast.parse(contents.encode(encoding), name)
     visitor = programslice.visitor.LineDependencyVisitor()
     visitor.visit(node)
     graph = visitor.get_graph_for(currentline)
-    return graph.slice_forward(currentline)
+    if graph:
+        lines = graph.slice_forward(currentline)
+    return lines

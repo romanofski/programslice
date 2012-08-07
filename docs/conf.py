43528224ceb41d812d873ed75a75ddbc5be5abe3
Roman Joost <roman@bromeco.de>
2012-08-07 15:59:14 +1000

    Different theme - looks better.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'programslice'
-copyright = u'2012, Ró'
+copyright = u'2012, Róman Joost'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -94,7 +94,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

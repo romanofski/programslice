6af805ce70e0998d7a115d64492dc43f735922af
Roman Joost <roman@bromeco.de>
2015-02-02 20:07:59 +1000

    tests: Added missing test.

---
+++ programslice/tests/testdata/overlapping.py
@@ -0,0 +1,15 @@
+def function():
+    a = 1
+    a = a + 1
+    return a
+
+
+def issue_1():
+    a = 1
+    b = 2
+    c = 2 * b
+    a = a - b
+    a = a + c
+    a = a - b
+    a = 5
+    return a

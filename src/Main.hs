a51fcc45243016cf6511c93b9a2d5dee60cc5e8b
Róman Joost <roman@bromeco.de>
2015-05-19 20:40:13 +1000

    main: scruffy little command line options
    This way we can check out the AST, as well as check what we convert

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,39 @@
 import Parse
+import Language.Python.Version2.Parser (parseModule)
 import Compiler.Hoopl
+import System.Console.GetOpt (usageInfo, ArgDescr(..), OptDescr(..), ArgOrder(..), getOpt)
+import System.Environment (getArgs)
+import Text.Show.Pretty
+
+
+data Flag = PrintAST | PrintIR
+
+options :: [OptDescr Flag]
+options =
+    [ Option "a" ["ast"] (NoArg PrintAST) "pretty prints AST"
+    , Option "i" ["ir"] (NoArg PrintIR) "prints transformed IR"
+    ]
+
+run :: [String] -> IO [Flag]
+run args =
+    case getOpt Permute options args of
+        (o, _, []) -> return o
+        (_, _, err) -> ioError (userError (concat err ++ usageInfo header options))
+
+header :: String
+header = "Usage: programslice [i|a]"
 
 
 main :: IO ()
 main = do
-    c <- getContents
-    mapM_ print (runSimpleUniqueMonad $ runWithFuel 0 (parse c))
+    args <- getArgs
+    flags <- run args
+    case flags of
+        (PrintIR : _) -> do
+            c <- getContents
+            mapM_ print (runSimpleUniqueMonad $ runWithFuel 0 (parse c))
+        (PrintAST : _) -> do
+            c <- getContents
+            let Right parsed = parseModule c []
+            putStrLn $ ppShow parsed
+        _ -> ioError (userError $ "No arguments provided. " ++ header)
